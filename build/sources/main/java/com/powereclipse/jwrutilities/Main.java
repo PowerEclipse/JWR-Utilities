58adef1fa8b75434bc61db675e59f5e4cb4b2478
knoxhack <hackofgod@gmail.com>
2017-01-02 15:35:10 -0500

    fixes

--- build/sources/main/java/com/powereclipse/jwrutilities/Main.java
+++ build/sources/main/java/com/powereclipse/jwrutilities/Main.java
@@ -3,14 +3,29 @@ package com.powereclipse.jwrutilities;
 import com.powereclipse.jwrutilities.common.ProxyCommon;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.VERSION_BUILD,dependencies = ModInfo.DEPENDENCIES, updateJSON = ModInfo.UpdateJSON)
 public class Main {
     
-    @SidedProxy(serverSide = ModInfo.ServerSide, clientSide = ModInfo.ClientSide)
-    @Mod.Instance("jwrutilities")
-    public static Main instance;
+    @SidedProxy(serverSide = "com.powereclipse.jwrutilities.common.ProxyCommon", clientSide = "com.powereclipse.jwrutilities.client.ProxyClient")
     public static ProxyCommon proxy;
+
+    
+    
+    @Mod.Instance("jwrutilities")
+    public static Main instance = new Main(); 
+    
+    
+    @EventHandler
+    public void preInit (FMLPreInitializationEvent event) {
+      
+
+        proxy.preInit();
+    
     }
 
+
+}
--- src/main/java/com/powereclipse/jwrutilities/Main.java
+++ src/main/java/com/powereclipse/jwrutilities/Main.java
@@ -7,13 +7,16 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.VERSION_BUILD,dependencies = ModInfo.DEPENDENCIES, updateJSON = ModInfo.UpdateJSON)
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.VERSION_BUILD,dependencies = ModInfo.DEPENDENCIES, updateJSON = "https://raw.githubusercontent.com/PowerEclipse/JWR-Utilities/master/update.json")
 public class Main {
     
-    @SidedProxy(serverSide = ModInfo.ServerSide, clientSide = ModInfo.ClientSide)
-    //@Mod.Instance("jwrutilities")
-    public static Main instance = new Main(); 
+    @SidedProxy(serverSide = "com.powereclipse.jwrutilities.common.ProxyCommon", clientSide = "com.powereclipse.jwrutilities.client.ProxyClient")
     public static ProxyCommon proxy;
+
+    
+    
+    @Mod.Instance("jwrutilities")
+    public static Main instance = new Main(); 
     
     
     @EventHandler
